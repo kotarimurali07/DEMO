5b77fdddcd05e2eb73b4760da5cdb2989a1a1567
Murali Kotari <83963142+kotarimurali07@users.noreply.github.com>
2021-06-22 14:56:24 +0530

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,12 +3,7 @@ import "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDocAAF-MJ8bNTXbpDHY3r_fW8oPwLdxjw",
-  authDomain: "slack-28304.firebaseapp.com",
-  projectId: "slack-28304",
-  storageBucket: "slack-28304.appspot.com",
-  messagingSenderId: "583976248013",
-  appId: "1:583976248013:web:940c8de43414f376137f84",
+
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
